ef542ae276e4055cb10dfd326a3e207b52d1eabb
danylmakogon <danyl.makogon@viada.de>
2024-12-19 17:06:03 +0100

    Add components for home page structure
    Implemented Navbar, Description, About, Advantages, Reviews, and Contacts components with respective HTML, CSS, and TypeScript files. Updated the main app component to integrate these new sections for better modularization.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from './home/navbar/navbar.component';
+import {DescriptionComponent} from './home/description/description.component';
+import {AboutComponent} from './home/about/about.component';
+import {AdvantagesComponent} from './home/advantages/advantages.component';
+import {ReviewsComponent} from './home/reviews/reviews.component';
+import {ContactsComponent} from './home/contacts/contacts.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, DescriptionComponent, AboutComponent, AdvantagesComponent, ReviewsComponent, ContactsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
